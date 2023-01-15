2e7902d31cc8a2e6bb1a2ad4d3fc2e12e86768b2
Anton Brisilin <brisilant@yandex.ru>
2023-01-15 23:22:45 +0300

    Update for 1.19.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,28 +24,23 @@ dependencies {
     val fabricKotlinVersion: String by project
     modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion)
 }
+
+val javaVersion = JavaLanguageVersion.of(17)
+java {
+    toolchain {
+        languageVersion.set(javaVersion)
+    }
+}
+kotlin {
+    jvmToolchain {
+        languageVersion.set(javaVersion)
+    }
+}
+
 tasks {
-    val javaVersion = JavaVersion.VERSION_17
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-        options.release.set(javaVersion.toString().toInt())
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions { jvmTarget = javaVersion.toString() }
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-    }
     jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
     processResources {
         inputs.property("version", project.version)
         filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
-    }
-    java {
-        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-        withSourcesJar()
     }
 }
