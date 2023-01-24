d7185b177398ef8acdbee0ae1a46dee85c51b2cb
Anton Brisilin <brisilant@yandex.ru>
2023-01-24 21:59:59 +0300

    Fix keybindings not present in settings

--- src/main/kotlin/ru/mexator/randbuild/RandBuildMod.kt
+++ src/main/kotlin/ru/mexator/randbuild/RandBuildMod.kt
@@ -1,18 +1,19 @@
 package ru.mexator.randbuild
 
+import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
-import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import ru.mexator.randbuild.keybindings.Keybindings
 import ru.mexator.randbuild.random.BlockRandomizer
 
 @Suppress("UNUSED")
 @Environment(EnvType.CLIENT)
-object RandBuildMod : ModInitializer {
+object RandBuildMod : ClientModInitializer {
     const val MOD_ID = "randbuild"
 
-    override fun onInitialize() {
+    override fun onInitializeClient() {
+        Keybindings.register()
         ClientTickEvents.END_CLIENT_TICK.register {
             if (Keybindings.resetSlotsBinding.wasPressed()) {
                 CheckedSlotsRegistry.clear()
--- src/main/kotlin/ru/mexator/randbuild/keybindings/Keybindings.kt
+++ src/main/kotlin/ru/mexator/randbuild/keybindings/Keybindings.kt
@@ -8,22 +8,29 @@ import org.lwjgl.glfw.GLFW
 object Keybindings {
     private const val RandbuildCategory = "Randbuild mod"
 
+    private var _toggleSlotBinding: KeyBinding? = null
+    private var _resetSlotsBinding: KeyBinding? = null
+    val toggleSlotBinding: KeyBinding get() = _toggleSlotBinding ?: notInitializedError()
+    val resetSlotsBinding: KeyBinding get() = _resetSlotsBinding ?: notInitializedError()
 
-    val toggleSlotBinding: KeyBinding = KeyBindingHelper.registerKeyBinding(
-        KeyBinding(
-            "key.lock_slot",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_LEFT_ALT,
-            RandbuildCategory
+    fun register() {
+        _toggleSlotBinding = KeyBindingHelper.registerKeyBinding(
+            KeyBinding(
+                "key.lock_slot",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_LEFT_ALT,
+                RandbuildCategory
+            )
         )
-    )
+        _resetSlotsBinding = KeyBindingHelper.registerKeyBinding(
+            KeyBinding(
+                "key.reset_locks",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                RandbuildCategory
+            )
+        )
+    }
 
-    val resetSlotsBinding: KeyBinding = KeyBindingHelper.registerKeyBinding(
-        KeyBinding(
-            "key.reset_locks",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_MOD_CONTROL or GLFW.GLFW_KEY_R, // Ctrl + R
-            RandbuildCategory
-        )
-    )
+    private fun notInitializedError(): Nothing = error("Accessing key bindings before mod is initialized")
 }
